b435cb6e3da9fa00e6b8cea967e90f825ceff853
MostafaELite <mostafaelite7@gmail.com>
2018-08-13 00:24:11 +0300

    - Adding HR Project

---
+++ HR/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using HR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Insert()        
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Employee newEmployee)
+        {
+            MyData mydatabase = new MyData();
+            mydatabase.Employees.Add(newEmployee);
+            mydatabase.SaveChanges();
+            return Content("Employee Inserted Successfully");
+        }
+
+
+
+    }
+}
